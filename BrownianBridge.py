f35e0b523756a5cd54a5c285e689d3ed20b13141
ZHCHU929 <151024034+ZHCHU929@users.noreply.github.com>
2023-11-17 01:31:46 +1100

    Update BrownianBridge.py

--- BrownianBridge.py
+++ BrownianBridge.py
@@ -14,20 +14,20 @@ B = 120
 M = 1000     # number of simulations
 
 # Function for Monte Carlo simulation
-def monte_carlo_simulation(N):
+def monte_carlo_simulation(N,a):
     dt = T/N
     nudt = (r - 0.5*vol**2)*dt
     volsdt = vol*np.sqrt(dt)
-
+    np.random.seed(a)
     sum_CT = 0
-    np.random.seed(0)
+
 
     for i in range(M):
         BARRIER = False
         St = S0
 
         for j in range(N):        
-            epsilon = np.random.normal()
+            epsilon = np.random.randn()
             Stn = St*np.exp(nudt + volsdt*epsilon)
             St = Stn
             if St >= H:
@@ -45,9 +45,9 @@ def monte_carlo_simulation(N):
     return C0
 
 # Function for Brownian Bridge simulation
-def brownian_bridge_simulation(N):
+def brownian_bridge_simulation(N,a):
     payoff_sum = 0.0
-    np.random.seed(0)
+    np.random.seed(a)
     for _ in range(M):
         S = S0
         P = S * np.exp((r - 0.5 * vol**2) * T + vol * np.sqrt(T) * np.random.randn())
@@ -57,18 +57,26 @@ def brownian_bridge_simulation(N):
     return np.exp(-r * T) * payoff_sum / M
 
 # Running simulations for different N values
-N_values = range(500, 5001, 100)
+N_values = range(100, 500, 10)
 mc_results = []
 bb_results = []
 differences = []
+mc = []
+bb = []
 
 for N in N_values:
-    mc_value = monte_carlo_simulation(N)
-    bb_value = brownian_bridge_simulation(N)
-    mc_results.append(mc_value)
-    bb_results.append(bb_value)
-    differences.append(abs(mc_value - bb_value))
+    for i in range (10):
+        mc_value = monte_carlo_simulation(N,i)
+        bb_value = brownian_bridge_simulation(N,i)
+        mc_results.append(mc_value)
+        bb_results.append(bb_value)
+    mc_mean = np.mean(mc_results)
+    bb_mean = np.mean(bb_results)
+    mc.append(mc_mean)
+    bb.append(bb_mean)
+    differences.append(abs(mc_mean - bb_mean))
 
+print(differences)
 # Linear regression for the differences
 slope, intercept, r_value, p_value, std_err = linregress(N_values, differences)
 
@@ -82,60 +90,11 @@ plt.title('Difference between Monte Carlo and Brownian Bridge Simulations')
 plt.legend()
 plt.show()
 
-
-
-import time
-import numpy as np
-import matplotlib.pyplot as plt
-S0 = 100      # initial stock price
-K = 100       # strike price
-T = 1         # time to maturity in years
-H = 125       # up-and-out barrier price/value
-r = 0.01      # annual risk-free rate
-vol = 0.2     # volatility (%)
-
-N = 100       # number of time steps
-M = 1000      # number of simulations
-
-start_time = time.time()
-
-# Precompute constants
-dt = T/N
-nudt = (r - 0.5*vol**2)*dt
-volsdt = vol*np.sqrt(dt)
-erdt = np.exp(r*dt)
-
-# Standard Error Placeholders
-sum_CT = 0
-sum_CT2 = 0
-
-# Monte Carlo Method
-for i in range(M):
-
-    # Barrier Crossed Flag
-    BARRIER = False
-    St = S0
-
-    for j in range(N):
-        epsilon = np.random.normal()
-        Stn = St*np.exp( nudt + volsdt*epsilon )
-        St = Stn
-        if St >= H:
-            BARRIER = True
-            break
-
-    if BARRIER:
-        CT = 0
-    else:
-        CT = max(0, K - St)
-
-    sum_CT = sum_CT + CT
-    sum_CT2 = sum_CT2 + CT*CT
-
-# Compute Expectation and SE
-C0 = np.exp(-r*T)*sum_CT/M
-sigma = np.sqrt( (sum_CT2 - sum_CT*sum_CT/M)*np.exp(-2*r*T) / (M-1) )
-SE = sigma/np.sqrt(M)
-
-print("Call value is ${0} with SE +/- {1}".format(np.round(C0,2),np.round(SE,3)))
-print("Computation time is: ", round(time.time() - start_time,4))
+plt.figure(figsize=(10, 6))
+plt.plot(N_values,  mc, 'o-', label='Monte Carlo')
+plt.plot(N_values, bb, 'o-', label='Brownian Bridge')
+plt.xlabel('Number of Steps (N)')
+plt.ylabel('average payoff')
+plt.title('Monte Carlo and Brownian Bridge Simulations')
+plt.legend()
+plt.show()
